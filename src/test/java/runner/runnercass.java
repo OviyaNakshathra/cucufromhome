48127de631432376fd421897927514d4cf6ef880
OviyaNakshathra <52442136+OviyaNakshathra@users.noreply.github.com>
2019-07-07 11:10:42 +0530

    Update runnercass.java

--- src/test/java/runner/runnercass.java
+++ src/test/java/runner/runnercass.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= {"src/test/resources/skeleton/case2.feature"},glue="skeleton",plugin="html:target/htmlReport")
+@CucumberOptions(features= {"src/test/resources/skeleton/case2.feature"},glue="skeleton",plugin="json:target/jsonReport.json")
 public class runnercass {
 
 }
